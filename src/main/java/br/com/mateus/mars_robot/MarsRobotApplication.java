eec3b0aa7b7b7a08d0aca441539f972276e14ef1
Mateus Firmino <mateusfirmino1@hotmail.com>
2024-07-18 11:43:25 -0400

    Initial Commit

---
+++ src/main/java/br/com/mateus/mars_robot/MarsRobotApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateus.mars_robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mateus/mars_robot/MarsRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateus.mars_robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
