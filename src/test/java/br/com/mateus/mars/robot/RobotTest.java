73812d8dec6e4bd33164915773d319c2aadd3ec0
Mateus Firmino <mateusfirmino1@hotmail.com>
2024-07-19 15:59:41 -0400

    add dockerfile and configuration

--- src/test/java/br/com/mateus/mars/robot/RobotTest.java
+++ src/test/java/br/com/mateus/mars/robot/RobotTest.java
@@ -1,18 +1,26 @@
 package br.com.mateus.mars.robot;
 
+import br.com.mateus.mars.robot.controller.RobotController;
 import br.com.mateus.mars.robot.model.Robot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
 
+@SpringBootTest
 public class RobotTest {
+
+  @Autowired
+  private RobotController robotController;
 
   @ParameterizedTest
   @MethodSource("validCommandProvider")
@@ -24,20 +32,25 @@ public class RobotTest {
     robot.processCommands(command);
     Assertions.assertThat(robot.toString()).isEqualTo(result);
   }
-//@ParameterizedTest
-//@NullSource
-//@EmptySource
-//@ValueSource(strings = "AAAAAAAAAAA")
-//  void test2(String command) {
-//    Robot robot = new Robot();
-//    Assertions.assertThatThrownBy(() -> robot.processCommands(command))
-//      .isInstanceOf(IllegalArgumentException.class).hasMessage("Invalid command: A");
-//  }
 
-  static Stream<Arguments> validCommandProvider(){
+  @Test
+  public void testMoveRight() {
+    ResponseEntity<String> response = robotController.moveRobot("MMRMMRMM");
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals("(2, 0, S)", response.getBody());
+  }
+
+  @Test
+  public void testMoveLeft() {
+    ResponseEntity<String> response = robotController.moveRobot("MML");
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals("(0, 2, W)", response.getBody());
+  }
+
+  static Stream<Arguments> validCommandProvider() {
     return Stream.of(
-      Arguments.of("MMRMMRMM","(2, 0, S)"),
-      Arguments.of("MML","(0, 2, W)")
+      Arguments.of("MMRMMRMM", "(2, 0, S)"),
+      Arguments.of("MML", "(0, 2, W)")
     );
   }
 
